2f4ee6d70f15a6cbc4d9ab0b9a40016309aa18af
Sean Wu <sean_wu@edge-core.com>
2021-11-17 00:49:18 +0000

    add q0371

---
+++ src/q0371.h
@@ -0,0 +1,26 @@
+
+using namespace std;
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum;
+        while(b!=0)
+        {
+            int c = a^b;
+            b = (unsigned int)(a&b) << 1;
+            a = c;
+        }
+        return a;
+    }
+};
+
+TEST(TestSolution, test1) {
+    Solution s;
+    EXPECT_EQ(s.getSum(1,2), 3);
+    EXPECT_EQ(s.getSum(2,3), 5);
+    EXPECT_EQ(s.getSum(-4,3), -1);
+    EXPECT_EQ(s.getSum(-4,-3), -7);
+    EXPECT_EQ(s.getSum(-1,1), 0);
+    EXPECT_EQ(s.getSum(1,-1), 0);
+}
