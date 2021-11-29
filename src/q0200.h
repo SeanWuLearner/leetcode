092fc834989627826bdb7c199255a40bfe8602cd
Sean Wu <sean_wu@edge-core.com>
2021-11-29 06:59:19 +0000

    add UnionFind solution to q0200

--- src/q0200.h
+++ src/q0200.h
@@ -1,4 +1,5 @@
-class Solution {
+/* Normal count and expansion answer */
+class Solution_MarkAndCnt {
 private:
     int m,n;
 public:
@@ -26,6 +27,83 @@ public:
             }
         }
         return num;
+    }
+};
+
+
+/* Union-find solution */
+class UnionFind{
+private:
+    int col_size;
+    int count;
+    vector<int> parent;
+    vector<int> rank;
+public:
+    int get1dIdx(int r, int c){
+        return r * col_size + c;
+    }
+    UnionFind(vector<vector<char>>& grid){
+        count = 0;
+        col_size = grid[0].size();
+        for(int i=0 ; i<grid.size() ; i++){
+            for(int j=0 ; j<grid[0].size() ; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    parent.push_back(get1dIdx(i, j));
+                    rank.push_back(1);
+                }else{
+                    parent.push_back(-1);
+                    rank.push_back(0);
+                }
+            }
+        }
+    }
+
+    int find(int r, int c){
+        int i = get1dIdx(r,c);
+        while(i != parent[i]){
+            parent[i] = parent[parent[i]];
+            i = parent[i];
+        }
+        return parent[i];
+    }
+
+    void Union(int r1, int c1, int r2, int c2){
+        int root1 = find(r1,c1), root2 = find(r2,c2);
+        if(root1 == root2)
+            return;
+
+        if(rank[root1] > rank[root2]){
+            parent[root2] = root1;
+            rank[root1] += rank[root2];
+        }else{
+            parent[root1] = root2;
+            rank[root2] += rank[root1];
+        }
+        count--;
+    }
+    int getCount(){
+        return count;
+    }
+};
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        UnionFind uf(grid);
+
+        for(int i=0 ; i<m ; i++){
+            for(int j=0; j<n ; j++){
+                if(grid[i][j]=='1'){
+                    if(i>0 && grid[i-1][j]=='1') uf.Union(i,j, i-1,j);
+                    if(j>0 && grid[i][j-1]=='1') uf.Union(i,j, i,j-1);
+                    if(i<m-1 && grid[i+1][j]=='1') uf.Union(i,j, i+1,j);
+                    if(j<n-1 && grid[i][j+1]=='1') uf.Union(i,j, i,j+1);
+                }
+            }
+        }
+        return uf.getCount();
     }
 };
 
